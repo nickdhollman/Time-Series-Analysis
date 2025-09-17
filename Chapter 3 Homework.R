ab0b00806d0e77998eb3a6123512581c2b6d6739
nickdhollman <nickdhollman@gmail.com>
2025-09-17 16:53:24 -0500

    Finished HW 3

--- Chapter 3 Homework.R
+++ Chapter 3 Homework.R
@@ -64,3 +64,85 @@ acf(rstudent(fit_const))
 # based on your answers in problem 1b.
 summary(linearmodel)
 summary(fit_const)
+
+### PROBLEM 2 ###
+# Load the google data in the TSA package and get mean, sample variance, and 95% CI
+data(google)
+plot(google)
+mean(google)
+var(google)                 
+n <- sum(!is.na(google))
+ybar <- mean(google)
+s2 <- var(google)        
+upper <- ybar + 2 * sqrt(s2 / n)  
+lower <- ybar - 2 * sqrt(s2 / n)  
+print(upper)
+print(lower)
+
+# construct a correlogram for the google data 
+acf(google)
+
+#### PROBLEM 3 ### 
+# Load the winnebago data set in the TSA package. Assume that Yt = µt + Xt
+# where Xt is a 0 mean stationary process and consider the following 2 models for µt:
+## Model 1: µt = β0 + β1t + β2t^2
+## Model 2: µt = β0 + β1t + β2t^2 + β3cos(2πt) + β4sin(2πt)
+data("winnebago")
+# model 1 specifications
+t=time(winnebago)
+t.sq = t*t
+model_one = lm(winnebago~t + t.sq)
+# model 2 specifications
+model_two = lm(winnebago~t + t.sq + cos(2*pi*t) + sin(2*pi*t))
+# plot model 1 + model 2
+plot(winnebago)
+abline(model_one, lwd = 2)
+summary(model_one)
+summary(model_two)
+# model 1 curve - dashed
+curve(model_one$coefficients[1] + model_one$coefficients [2]*x +
+      model_one$coefficients [3]*x^2, add=TRUE, lwd = 2, lty = 2)
+# model 2 curve - solid
+curve(model_two$coefficients[1]+ model_two$coefficients [2]*x + 
+         model_two$coefficients [3]*x^2 +
+         model_two$coefficients [4]*cos(2*pi*x)+
+         model_two$coefficients[5]*sin(2*pi*x) ,add=TRUE, lwd = 2, lty = 1)
+legend("topleft",
+       legend = c("model 1 (B0 + B1t + B2t^2)",
+                  "model 2 (B0 + B1t + B2t^2 + B3cos(2*pi*t) + B4sin(2*pi*t)"),
+       lty = c(2,1), lwd = 2, bty = "n")
+
+# b) Provide four plots (one for each assumption) for each model and use them to assess the assumption that Xt
+# i. has mean 0,
+plot(y=rstudent(model_one), x=as.vector(time(winnebago)), xlab = "Time", ylab="Stand. Resid.", type = "o")
+plot(y=rstudent(model_two), x=as.vector(time(winnebago)), xlab = "Time", ylab="Stand. Resid.", type = "o")
+
+# ii. has constant variance
+plot(y=rstudent(model_one), x = as.vector(fitted(model_one)), xlab=" mu hat ", ylab="Stand. Resid")
+plot(y=rstudent(model_two), x = as.vector(fitted(model_two)), xlab=" mu hat ", ylab="Stand. Resid")
+
+# iii. is normally distributed
+hist(rstudent(model_one), xlab = "Stand. Residuals")
+qqnorm(rstudent(model_one))
+abline(0,1)
+hist(rstudent(model_two), xlab = "Stand. Residuals")
+qqnorm(rstudent(model_two))
+abline(0,1)
+
+# iv. is white noise.
+acf(rstudent(model_one))
+acf(rstudent(model_two))
+
+
+
+
+
+
+
+
+
+
+
+
+
+       
